a771c53fa36ad4cceb326f425581eae5fe2d01b4
thomas434 <tomas.temo77@gmail.com>
2021-07-27 19:23:20 +0200

    try django cookiecutter

---
+++ trycookie/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from trycookie.users.models import User
+from trycookie.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
